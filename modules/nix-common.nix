35cdb3b0c8f0912a200cee1404fc8c3d08b23807
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-03-25 18:05:01 +0100

    Use binary cache

--- modules/nix-common.nix
+++ modules/nix-common.nix
@@ -4,6 +4,10 @@
 
   nix = {
 
+    binaryCachePublicKeys = ["cache.lounge.rocks:uXa8UuAEQoKFtU8Om/hq6d7U+HgcrduTVr8Cfl6JuaY="];
+    binaryCaches = [ "https://cache.lounge.rocks" ];
+    trustedBinaryCaches =  ["https://cache.lounge.rocks"];
+
     # Save space by hardlinking store files
     autoOptimiseStore = true;
 
