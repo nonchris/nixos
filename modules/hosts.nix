092ecd593fea92c03ca7f934408a9280aee1adaa
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-03-28 23:55:19 +0200

    add rick to hosts.nix

--- modules/hosts.nix
+++ modules/hosts.nix
@@ -5,10 +5,12 @@
     extraHosts = ''
       #
       192.168.88.1 unifi
+      192.168.88.17 rick
       192.168.5.15 vsphere
       
       # home
       192.168.88.1 unifi.home
+      192.168.88.17 rick.home
 
       # local
       192.168.5.15 vsphere.local
