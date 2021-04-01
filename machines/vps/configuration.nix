368e5866dcc874a4b7ca3cbed4f1992322e02cae
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-04-01 16:39:53 +0200

    Create new machine vps

---
+++ machines/vps/configuration.nix
@@ -0,0 +1,48 @@
+{ config, lib, pkgs, ... }:
+let
+  mayniklas = builtins.fetchGit {
+    # Updated 2020-04-01
+    url = "https://github.com/mayniklas/nixos";
+    rev = "d4fa719e8a3b7bc7d6e2ddde7b4c84e1a011becf";
+  };
+  home = builtins.fetchGit {
+    url = "https://github.com/nonchris/nixos-home";
+    ref = "main";
+  };
+  home-manager = builtins.fetchGit {
+    url = "https://github.com/nix-community/home-manager.git";
+    ref = "master";
+  };
+
+in {
+  imports = [
+
+    # Users
+    ../../users/chris.nix
+    ../../users/root.nix
+
+    # Modules imported from MayNiklas
+    "${mayniklas}/modules/docker.nix"
+    "${mayniklas}/modules/locale.nix"
+    "${mayniklas}/modules/openssh.nix"
+    "${mayniklas}/modules/kvm-guest.nix"
+    "${mayniklas}/modules/options.nix"
+    "${mayniklas}/modules/zsh.nix"
+
+    # Modules
+    ../../modules/nix-common.nix
+
+    # home-manager
+    (import "${home-manager}/nixos")
+  ];
+
+  mainUser = "chris";
+  mainUserHome = "${config.users.extraUsers.${config.mainUser}.home}";
+
+  home-manager.users.chris = import "${home}/home-server.nix";
+
+  networking = { hostName = "vps"; };
+
+  environment.systemPackages = with pkgs; [ bash-completion git nixfmt wget ];
+
+}
