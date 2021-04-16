30f8441a4229d3e9bafcbbb5af0045824aba7b04
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-04-16 20:02:39 +0200

    update MayNiklas options being used for rick

--- machines/rick/configuration.nix
+++ machines/rick/configuration.nix
@@ -1,9 +1,9 @@
 { config, lib, pkgs, ... }:
 let
   mayniklas = builtins.fetchGit {
-    # Updated 2020-03-28
+    # Updated 2020-04-16
     url = "https://github.com/mayniklas/nixos";
-    rev = "c0cda4acecaac8b4d335d6d82f92e7c39d3aa41b";
+    rev = "2710a8adf4e74f3d2e396aae5b3259612ae91766";
   };
   home-manager = builtins.fetchGit {
     url = "https://github.com/nix-community/home-manager.git";
@@ -18,12 +18,12 @@ in {
     ../../users/root.nix
 
     # Modules imported from MayNiklas
-    "${mayniklas}/modules/docker.nix"
-    "${mayniklas}/modules/locale.nix"
-    "${mayniklas}/modules/openssh.nix"
-    "${mayniklas}/modules/options.nix"
-    "${mayniklas}/modules/vmware-guest.nix"
-    "${mayniklas}/modules/zsh.nix"
+    "${mayniklas}/modules/docker"
+    "${mayniklas}/modules/locale"
+    "${mayniklas}/modules/openssh"
+    "${mayniklas}/modules/options"
+    "${mayniklas}/modules/vmware-guest"
+    "${mayniklas}/modules/zsh"
 
     # Modules
     ../../modules/hosts
@@ -38,9 +38,6 @@ in {
     (import "${home-manager}/nixos")
   ];
 
-  mainUser = "chris";
-  mainUserHome = "${config.users.extraUsers.${config.mainUser}.home}";
-
   home-manager.users.chris = { imports = [ ../../home-manager/home-server.nix ]; };
 
   networking = { hostName = "rick"; };
@@ -50,6 +47,15 @@ in {
   nonchris = {
     thelounge.enable = true;
     common.enable = true;
+  };
+
+  mayniklas = {
+    var.mainUser = "chris";
+    docker.enable = true;
+    locale.enable = true;
+    openssh.enable = true;
+    vmware-guest.enable = true;
+    zsh.enable = true;
   };
 
 }
