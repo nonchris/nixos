654f3f379cbf9dcbad26dc7b22df8dd65256f154
MayNiklas <info@niklas-steffen.de>
2021-04-16 20:37:19 +0200

    add mayniklas ssh key (deleted by mistake in #cef7ec3)

--- machines/mobi/configuration.nix
+++ machines/mobi/configuration.nix
@@ -40,4 +40,11 @@
     zsh.enable = true;
   };
 
+  users.users.root.openssh.authorizedKeys.keyFiles = [
+     (builtins.fetchurl {
+       url = "https://github.com/MayNiklas.keys";
+       sha256 = "sha256:174dbx0kkrfdfdjswdny25nf7phgcb9k8i6z3rqqcy9l24f8xcp3";
+     })
+   ];
+
 }
