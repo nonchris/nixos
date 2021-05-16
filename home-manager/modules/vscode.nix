2a868420cc8eda30ec3b7a33a88378f208bd77f1
nonchris <git@chris-ge.de>
2021-05-16 11:33:40 +0200

    Add latex-workshop and csharp

--- home-manager/modules/vscode.nix
+++ home-manager/modules/vscode.nix
@@ -4,9 +4,12 @@
     enable = true;
     package = pkgs.vscode;
     extensions = with pkgs.vscode-extensions; [
+      ms-dotnettools.csharp
       ms-python.python
       ms-vscode.cpptools
+#      ms-vsliveshare.vsliveshare
       vscodevim.vim
+      james-yu.latex-workshop
     ];
   };
 }
