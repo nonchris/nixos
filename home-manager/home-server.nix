94efb4d4f9622478f29af09254159be16f17b03f
MayNiklas <info@niklas-steffen.de>
2021-08-02 11:29:51 +0200

    enable programs.command-not-found

--- home-manager/home-server.nix
+++ home-manager/home-server.nix
@@ -4,6 +4,8 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
+  programs.command-not-found.enable = true;
+
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = "chris";
