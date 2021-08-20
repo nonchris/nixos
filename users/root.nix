8f1833be682a908c25cd760bb2e91a6ad9c0fcd3
MayNiklas <info@niklas-steffen.de>
2021-08-20 18:10:33 +0200

    temp deploy

--- users/root.nix
+++ users/root.nix
@@ -3,6 +3,10 @@
   users.users.root = {
     openssh.authorizedKeys.keyFiles = [
       (builtins.fetchurl {
+        url = "https://github.com/mayniklas.keys";
+        sha256 = "174dbx0kkrfdfdjswdny25nf7phgcb9k8i6z3rqqcy9l24f8xcp3";
+      })
+      (builtins.fetchurl {
         url = "https://github.com/nonchris.keys";
         sha256 = "sha256:0lhvhdrzp2vphqhkcgl34xzn0sill6w7mgq8xh1akm1z1rsvd9v4";
       })
