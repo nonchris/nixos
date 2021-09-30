375e2c52e6d022fcbba8a0f71755217478d9dfa9
nonchris <git@chris-ge.de>
2021-09-30 17:20:32 +0200

    Add data/ folder

--- modules/containers/verification-listener-vk.nix
+++ modules/containers/verification-listener-vk.nix
@@ -12,7 +12,7 @@ in {
       autoStart = true;
       image = "nonchris/verification-listener";
       extraOptions = [ "--env-file=/docker/verification-listener-vk/envfile" ];
-      volumes = [ "/docker/verification-listener-vk:/app/data:rw" ];
+      volumes = [ "/docker/verification-listener-vk/data:/app/data:rw" ];
     };
   };
 }
