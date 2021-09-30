19715a03b358a5c3cfb4ab7a2df9b19c7d8a5a81
nonchris <git@chris-ge.de>
2021-09-30 17:09:16 +0200

    Add nonchris.verification-listener-vk option

--- modules/containers/verification-listener-vk.nix
+++ modules/containers/verification-listener-vk.nix
@@ -1,8 +1,18 @@
-{ config, pkgs, ... }: {
-  virtualisation.oci-containers.containers.verification-listener = {
-    autoStart = true;
-    image = "nonchris/verification-listener";
-    extraOptions = [ "--env-file=/docker/verification-listener-vk/envfile" ];
-    volumes = [ "/docker/verification-listener-vk:/app/data:rw" ];
+{ lib, config, pkgs, ... }:
+with lib;
+let cfg = config.nonchris.verification-listener-vk;
+in {
+  options.nonchris.verification-listener-vk = {
+    enable = mkEnableOption "deploy verification listener";
+  };
+
+  config = mkIf cfg.enable {
+
+    virtualisation.oci-containers.containers.verification-listener-vk = {
+      autoStart = true;
+      image = "nonchris/verification-listener";
+      extraOptions = [ "--env-file=/docker/verification-listener-vk/envfile" ];
+      volumes = [ "/docker/verification-listener-vk:/app/data:rw" ];
+    };
   };
 }
