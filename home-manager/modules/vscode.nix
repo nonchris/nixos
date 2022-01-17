067b295cd7f8019395d616e974239bb0ac62592f
nonchris <git@chris-ge.de>
2022-01-17 14:42:42 +0100

    Update plugins for vscode

--- home-manager/modules/vscode.nix
+++ home-manager/modules/vscode.nix
@@ -4,12 +4,17 @@
     enable = true;
     package = pkgs.vscode;
     extensions = with pkgs.vscode-extensions; [
-      ms-dotnettools.csharp
+      # bbenoist.Nix
+      brettm12345.nixfmt-vscode
+      # lextudio.restructuredtext  # not existing
+      # ms-dotnettools.csharp
       ms-python.python
       ms-vscode.cpptools
+      ms-vscode-remote.remote-ssh
       ms-vsliveshare.vsliveshare
       #      vscodevim.vim
       james-yu.latex-workshop
+      # spmeesseman.vscode-taskexplorer  # not existing
       zhuangtongfa.material-theme
     ];
   };
