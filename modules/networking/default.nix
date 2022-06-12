3fec34eae1572bced9b5ee72f615a55ffa7c4537
Niklas <44636701+MayNiklas@users.noreply.github.com>
2022-06-12 12:51:48 +0200

    Don't use local DNS servers

--- modules/networking/default.nix
+++ modules/networking/default.nix
@@ -11,7 +11,7 @@ in {
 
     networking = {
       # Define the DNS servers
-      nameservers = [ "192.168.88.10" "192.168.88.1" "1.1.1.1" ];
+      nameservers = [ "1.1.1.1" ];
 
       # Enable networkmanager
       networkmanager.enable = true;
