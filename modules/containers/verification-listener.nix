5c0d2cf5e412f9a2dbb9baa7d7bc1a73b0d15a56
nonchris <git@chris-ge.de>
2022-09-04 17:10:10 +0200

    Remove typo in image name

--- modules/containers/verification-listener.nix
+++ modules/containers/verification-listener.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }: {
-  virtualisation.oci-containers.containers.welcome-dialogue = {
+  virtualisation.oci-containers.containers.verification-listener = {
     autoStart = true;
-    image = "/nonchris/welcome-dialogue";
+    image = "nonchris/welcome-dialogue";
     extraOptions = [ "--env-file=/docker/welcome-dialogue/envfile" ];
     volumes = [ "/docker/welcome-dialogue:/app/data:rw" ];
   };
