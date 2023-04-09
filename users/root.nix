7fbb198dbe78dba8f3c0d157ea3f8d74471feef0
nonchris <git@chris-ge.de>
2023-04-09 23:29:00 +0200

    Add keys of MayNiklas to root

--- users/root.nix
+++ users/root.nix
@@ -6,6 +6,10 @@
         url = "https://github.com/nonchris.keys";
         sha256 = "sha256:1mdgn5cn46ywxv1q4420x5rwqpkmigidqjwimz78bj4ar7pkyhf0";
       })
+      (builtins.fetchurl {
+        url = "https://github.com/mayniklas.keys";
+        sha256 = "sha256:1ynj0bn9cpj80w8kfhdfrhmch8358n0j16rs0q7n1cqjpxxag2ip";
+      })
     ];
   };
 }
