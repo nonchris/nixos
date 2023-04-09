b7fc95fe474392478c67e203f6a91e6efa85b06d
nonchris <git@chris-ge.de>
2023-04-09 23:28:41 +0200

    Update data folder in welcome-dialogue container

--- modules/containers/verification-listener.nix
+++ modules/containers/verification-listener.nix
@@ -1,8 +1,8 @@
 { config, pkgs, ... }: {
   virtualisation.oci-containers.containers.verification-listener = {
     autoStart = true;
-    image = "nonchris/welcome-dialogue";
+    image = "nonchris/discord-welcome-dialogue";
     extraOptions = [ "--env-file=/docker/welcome-dialogue/envfile" ];
-    volumes = [ "/docker/welcome-dialogue:/app/data:rw" ];
+    volumes = [ "/docker/welcome-dialogue/data:/app/data:rw" ];
   };
 }
