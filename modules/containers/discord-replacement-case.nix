a5d46cb96ad87746137a4b27eaa895f3c4c2570c
Chris G <git@chris-ge.de>
2023-04-25 22:26:12 +0200

    remove trailing minus

--- modules/containers/discord-replacement-case.nix
+++ modules/containers/discord-replacement-case.nix
@@ -1,5 +1,5 @@
 { config, pkgs, ... }: {
-  virtualisation.oci-containers.containers.discord-replacement-case- = {
+  virtualisation.oci-containers.containers.discord-replacement-case = {
     autoStart = true;
     image = "nonchris/discord-replacement-case";
     extraOptions = [ "--env-file=/docker/discord-replacement-case/envfile" ];
