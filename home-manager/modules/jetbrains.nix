0534189c7e9fe2310bd8454b7dee445d1ab42b92
MayNiklas <info@niklas-steffen.de>
2023-05-03 14:37:42 +0200

    remove pycharm mod

--- home-manager/modules/jetbrains.nix
+++ home-manager/modules/jetbrains.nix
@@ -5,21 +5,25 @@
     # rider
     # ruby-mine
     idea-ultimate
-    (pycharm-professional.override {
-      python3 = (pkgs.unstable.python3.withPackages
-        (p: with p; [
-          pip
-          setuptools
-          fastapi
-          matplotlib
-          multipart
-          numpy
-          pandas
-          requests
-          scipy
-          uvicorn
-        ]));
-    })
+    pycharm-professional
+
+    # # I want to try this out in the future.
+    # # it might (!) do something...
+    # (pycharm-professional.override {
+    #   python3 = (pkgs.unstable.python3.withPackages
+    #     (p: with p; [
+    #       pip
+    #       setuptools
+    #       fastapi
+    #       matplotlib
+    #       multipart
+    #       numpy
+    #       pandas
+    #       requests
+    #       scipy
+    #       uvicorn
+    #     ]));
+    # })
 
     # see https://github.com/NixOS/nixpkgs/pull/201518
     # see https://github.com/not-matthias/dotfiles-nix/issues/23
