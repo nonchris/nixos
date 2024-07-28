5d46fde4f1e9f431ece778368a1e4953e788602b
nonchris <git@chris-ge.de>
2024-07-29 00:38:13 +0200

    add jetbrains gateway

--- home-manager/modules/jetbrains.nix
+++ home-manager/modules/jetbrains.nix
@@ -4,6 +4,7 @@
     jdk
 
     clion
+    gateway
     # rider
     # ruby-mine
     idea-ultimate
@@ -21,6 +22,7 @@
     pc = "${pkgs.jetbrains.pycharm-professional}/bin/pycharm-professional . 2> /dev/null 1> /dev/null &";
     cl = "${pkgs.jetbrains.clion}/bin/clion . 2> /dev/null 1> /dev/null &";
     ij = "${pkgs.jetbrains.idea-ultimate}/bin/idea-ultimate . 2> /dev/null 1> /dev/null &";
+    gw = "${pkgs.jetbrains.gateway}/bin/gateway 2> /dev/null 1> /dev/null &";
   };
 
 }
