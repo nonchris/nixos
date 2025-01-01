18fb46b7eb7677780f819ca356700fb9fcc3b689
MayNiklas <info@niklas-steffen.de>
2025-01-02 00:09:26 +0100

    pkgs.ddccontrol seems currently broken

--- modules/ddcci/default.nix
+++ modules/ddcci/default.nix
@@ -17,27 +17,27 @@ in {
     boot.extraModulePackages = with config.boot.kernelPackages; [ ddcci-driver ];
 
     # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/services/hardware/ddccontrol.nix
-    services.ddccontrol.enable = true;
+    # services.ddccontrol.enable = true;
 
     environment.systemPackages = [ pkgs.i2c-tools ];
 
     home-manager.users.chris = {
       home.packages = [
-        (pkgs.writeShellScriptBin "lb" ''
-          # lower brightness
-          ${pkgs.ddccontrol}/bin/ddccontrol dev:/dev/i2c-8 -r 0x10 -W -${toString cfg.brightness_step}
-          ${pkgs.ddccontrol}/bin/ddccontrol dev:/dev/i2c-9 -r 0x10 -W -${toString cfg.brightness_step}
-        '')
-        (pkgs.writeShellScriptBin "hb" ''
-          # higher brightness
-          ${pkgs.ddccontrol}/bin/ddccontrol dev:/dev/i2c-8 -r 0x10 -W ${toString cfg.brightness_step}
-          ${pkgs.ddccontrol}/bin/ddccontrol dev:/dev/i2c-9 -r 0x10 -W ${toString cfg.brightness_step}
-        '')
-        (pkgs.writeShellScriptBin "sb" ''
-          # set brightness
-          ${pkgs.ddccontrol}/bin/ddccontrol dev:/dev/i2c-8 -r 0x10 -w $1
-          ${pkgs.ddccontrol}/bin/ddccontrol dev:/dev/i2c-9 -r 0x10 -w $1
-        '')
+        # (pkgs.writeShellScriptBin "lb" ''
+        #   # lower brightness
+        #   ${pkgs.ddccontrol}/bin/ddccontrol dev:/dev/i2c-8 -r 0x10 -W -${toString cfg.brightness_step}
+        #   ${pkgs.ddccontrol}/bin/ddccontrol dev:/dev/i2c-9 -r 0x10 -W -${toString cfg.brightness_step}
+        # '')
+        # (pkgs.writeShellScriptBin "hb" ''
+        #   # higher brightness
+        #   ${pkgs.ddccontrol}/bin/ddccontrol dev:/dev/i2c-8 -r 0x10 -W ${toString cfg.brightness_step}
+        #   ${pkgs.ddccontrol}/bin/ddccontrol dev:/dev/i2c-9 -r 0x10 -W ${toString cfg.brightness_step}
+        # '')
+        # (pkgs.writeShellScriptBin "sb" ''
+        #   # set brightness
+        #   ${pkgs.ddccontrol}/bin/ddccontrol dev:/dev/i2c-8 -r 0x10 -w $1
+        #   ${pkgs.ddccontrol}/bin/ddccontrol dev:/dev/i2c-9 -r 0x10 -w $1
+        # '')
       ];
     };
 
