cfdceeac2fff77abd82ee1454284f528c84a2ed4
nonchris <git@chris-ge.de>
2025-01-01 16:08:44 +0100

    does this fix the error 'error: 'nixFlakes' has been renamed to/replaced by 'nixVersions.stable'?' let's see :)

--- modules/nix-common/default.nix
+++ modules/nix-common/default.nix
@@ -25,7 +25,7 @@ in {
     nixpkgs = { config.allowUnfree = true; };
 
     nix = {
-      package = pkgs.nixFlakes;
+      package = pkgs.nixVersions.stable;
       extraOptions = ''
         # this enables the technically experimental feature Flakes
         experimental-features = nix-command flakes
