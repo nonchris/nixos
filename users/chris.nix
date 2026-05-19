08b721246dbd57aecb94c845fc4c49cd6280c5dd
MayNiklas <info@niklas-steffen.de>
2026-05-19 13:35:01 +0200

    update nonchris keys

--- users/chris.nix
+++ users/chris.nix
@@ -7,7 +7,7 @@
     openssh.authorizedKeys.keyFiles = [
       (builtins.fetchurl {
         url = "https://github.com/nonchris.keys";
-        sha256 = "sha256:02352mgrlxm69gh742gpr1miciz7qj9ym6a3sg14bmbhz403ypfy";
+        sha256 = "sha256:1kzr399yikf9jffyijrjxjvadh8r7pvinix3hwsq2hpdr0wr7pdp";
       })
     ];
   };
